35376f5b356096e56db56c15c2d6fb485ba47d93
JaviMerino-11 <javier.merinochica.practicas@telefonica.com>
2022-04-20 12:10:11 +0200

    Creo que buena pinta con handler_post

--- app_3.py
+++ app_3.py
@@ -1,22 +1,39 @@
-from aiohttp import web
+import json
+
+import aiohttp
 import requests
+from aiohttp import web
 
 
 async def handler_get(request):
     numero = request.match_info.get('number')
-    url = "https://rickandmortyapi.com/api/character/"+numero
+    url = "https://rickandmortyapi.com/api/character/" + numero
     response = requests.get(url)
     data = response.json()
     return web.Response(text=data['name'])
 
 
+async def handler_post(request):
+    url = "http://0.0.0.0:8080/post"
+    header={
+    'Accept': 'application/json',
+    'Content-Type': 'multipart/form-data'
+    }
+    body_modificado = {
+        'FirstName': 'Javier',
+        'LastName': 'Merino'
+    }
+
+    response = requests.post(url, data=json.dumps(body_modificado),headers=header)
+    return web.Response(text=response)
 
 
 app = web.Application()
 
 app.add_routes(
     [
-        web.get('/{number}', handler_get)
+        web.get('/{number}', handler_get),
+        web.post('/post', handler_post)
     ]
 )
 
